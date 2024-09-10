c0a6ec35b647a70e2a7a78bc1fd9adaa310a1476
khanh <ngiakhanh18@gmail.com>
2024-09-10 14:46:03 -0500

    cors2

--- app.js
+++ app.js
@@ -15,8 +15,20 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -32,17 +44,5 @@ async function connectToDatabase() {
 
 connectToDatabase();
 const allowedOrigins = ["https://bounce-rouge.vercel.app/"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
 
 module.exports = app;
